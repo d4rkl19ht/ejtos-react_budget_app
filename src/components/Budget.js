3d21f221cd35d26e043e392527b9fabae9e54501
oliver <d4rkl19ht79@gmail.com>
2023-12-06 05:03:51 +0800

    committing changes to origin

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -7,14 +7,25 @@ const Budget = () => {
     const handleBudgetChange = (event) => {
         if(event.target.value > 20000) {
             alert("The budget cannot exceed 20000.");
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: 20000
+            });
             return;
         }
 
         let totalExp = expenses.reduce((total,expense)=>total+=expense.cost,0)
         if(event.target.value < totalExp) {
             alert("You cannot reduce the budget value lower than the spending.")
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: totalExp
+            });
+            return;
         }
+    }
 
+    const dispatchEvent = (event)=>{
         dispatch({
             type: 'SET_BUDGET',
             payload: event.target.value
@@ -24,7 +35,7 @@ const Budget = () => {
         <div className="col me-1 alert alert-secondary">
             <div className="row">
                 <div className="col-5"><label for="budget" className="form-label mt-2">Budget:   {currency}</label></div>
-                <div className="col-7"><input type="number" className="form-control"  step="10" value={budget} onChange={handleBudgetChange} name="budget" id="budget"></input></div>
+                <div className="col-7"><input type="number" className="form-control"  step="10" value={budget} onChange={dispatchEvent} onBlur={handleBudgetChange} name="budget" id="budget"></input></div>
             </div>
         </div>
     );
